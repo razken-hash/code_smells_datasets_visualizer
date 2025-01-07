9eb5202ebce801bd8416e1c91205a71789777b6b
razken-hash <ka_kenniche@esi.dz>
2025-01-07 23:20:47 +0100

    Convert File Content Into JSON

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -22,6 +24,27 @@ class HomeScreen extends StatelessWidget {
               FilePickerResult? result = await FilePicker.platform.pickFiles();
               if (result != null) {
                 File file = File(result.files.single.path!);
+                if (!result.files.single.path!.endsWith(".json")) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      duration: Duration(seconds: 1),
+                      backgroundColor: Colors.red,
+                      content: Center(
+                        child: Text(
+                          "PLEASE, UPLOAD A JSON DATASET.",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  final content = await file.readAsString();
+                  final dataset = jsonDecode(content);
+                  print(dataset[0]);
+                }
               } else {
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
