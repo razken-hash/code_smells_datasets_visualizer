cde9fff0fb061ce2d38e358a5b5afb2389f291e3
razken-hash <ka_kenniche@esi.dz>
2025-01-20 21:12:03 +0100

    Code Smells List Added :: Needs More Colors & Filtering

--- lib/models/code_smell.dart
+++ lib/models/code_smell.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 import 'dart:ui';
 
+import 'package:flutter/foundation.dart';
+
 class CodeSmell {
+  final int id;
   final String name;
-  final Color itemColor;
+  final List<Color> itemColor;
   final Color textColor;
   bool isEnabled;
   CodeSmell({
+    required this.id,
     required this.name,
     required this.itemColor,
     required this.textColor,
@@ -14,12 +18,14 @@ class CodeSmell {
   });
 
   CodeSmell copyWith({
+    int? id,
     String? name,
-    Color? itemColor,
+    List<Color>? itemColor,
     Color? textColor,
     bool? isEnabled,
   }) {
     return CodeSmell(
+      id: id ?? this.id,
       name: name ?? this.name,
       itemColor: itemColor ?? this.itemColor,
       textColor: textColor ?? this.textColor,
@@ -29,8 +35,9 @@ class CodeSmell {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id': id,
       'name': name,
-      'itemColor': itemColor.value,
+      'itemColor': itemColor.map((x) => x.value).toList(),
       'textColor': textColor.value,
       'isEnabled': isEnabled,
     };
@@ -38,8 +45,13 @@ class CodeSmell {
 
   factory CodeSmell.fromMap(Map<String, dynamic> map) {
     return CodeSmell(
+      id: map['id'] as int,
       name: map['name'] as String,
-      itemColor: Color(map['itemColor'] as int),
+      itemColor: List<Color>.from(
+        (map['itemColor'] as List<int>).map<Color>(
+          (x) => Color(x),
+        ),
+      ),
       textColor: Color(map['textColor'] as int),
       isEnabled: map['isEnabled'] as bool,
     );
@@ -52,22 +64,24 @@ class CodeSmell {
 
   @override
   String toString() {
-    return 'CodeSmell(name: $name, itemColor: $itemColor, textColor: $textColor, isEnabled: $isEnabled)';
+    return 'CodeSmell(id: $id, name: $name, itemColor: $itemColor, textColor: $textColor, isEnabled: $isEnabled)';
   }
 
   @override
   bool operator ==(covariant CodeSmell other) {
     if (identical(this, other)) return true;
 
-    return other.name == name &&
-        other.itemColor == itemColor &&
+    return other.id == id &&
+        other.name == name &&
+        listEquals(other.itemColor, itemColor) &&
         other.textColor == textColor &&
         other.isEnabled == isEnabled;
   }
 
   @override
   int get hashCode {
-    return name.hashCode ^
+    return id.hashCode ^
+        name.hashCode ^
         itemColor.hashCode ^
         textColor.hashCode ^
         isEnabled.hashCode;
--- lib/repository/code_smells_repository.dart
+++ lib/repository/code_smells_repository.dart
@@ -1,23 +1,85 @@
 import 'package:code_smells_datasets_visualizer/models/code_smell.dart';
-import 'package:code_smells_datasets_visualizer/models/code_smell_item.dart';
 import 'package:flutter/material.dart';
 
 class DatasetRepository {
-  static List<CodeSmell> codeSmells = [];
 
-  static Map<String, Color> codeSmellsColors = {
-    "Divergent Change": Colors.black,
-    "Shotgun Surgery": Colors.yellow,
-    "Lazy Element": Colors.brown,
-    "Speculative Generality": Colors.orange,
-    "Message Chain": Colors.teal,
-    "Middle Man": Colors.purple,
-    "Insider Trading": Colors.pink,
-    "Alternative Classes": Colors.limeAccent,
-    "God Class": Colors.red,
-    "Data Class": Colors.green,
-    "Refused Bequest": Colors.blue,
-  };
-
-  static List<String> allCodeSmells = codeSmellsColors.keys.toList();
+  static List<CodeSmell> codeSmells = [
+    CodeSmell(
+      id: 1,
+      name: "God Class",
+      itemColor: [const Color(0xFF0575e6), const Color(0xFF00f260)],
+      textColor: Colors.black,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 2,
+      name: "Data Class",
+      itemColor: [const Color(0xFFDE00E2), const Color(0xFF4A00E0)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 3,
+      name: "Lazy Element",
+      itemColor: [Colors.red, Colors.purple],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 4,
+      name: "Message Chain",
+      itemColor:  [const Color(0xFF2C5364), const Color(0xFF0f2027)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 5,
+      name: "Middle Man",
+      itemColor:  [const Color(0xFFf5af19), const Color(0xFFf12711)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 6,
+      name: "Insider Trading",
+      itemColor:  [const Color(0xFF1CB5E0), const Color(0xFF000046)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 7,
+      name: "Refused Bequest",
+      itemColor:   [const Color(0xFF24fe41), const Color(0xFFFFFF00)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 8,
+      name: "Shotgun Surgery",
+      itemColor: [const Color(0xFFDE00E2), const Color(0xFF4A00E0)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 9,
+      name: "Divergent Change",
+      itemColor: [const Color(0xFF1CB5E0), const Color(0xFF000046)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 10,
+      name: "Speculative Generality",
+      itemColor: [const Color(0xFFf5af19), const Color(0xFFf12711)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+    CodeSmell(
+      id: 11,
+      name: "Alternative Classes",
+      itemColor: [const Color(0xFF0575e6), const Color(0xFF00f260)],
+      textColor: Colors.white,
+      isEnabled: true,
+    ),
+  ];
 }
